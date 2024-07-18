12da92dc98eb75afb62dd3fe3e971d9124087cdb
Gilberto Junior <giljrsantos@gmail.com>
2024-07-18 16:10:44 -0300

    feat: Criando o Formulário

--- src/app/categories/new/page.tsx
+++ src/app/categories/new/page.tsx
@@ -5,7 +5,18 @@ export default function page(){
     return (
         <div>
             <h2>Nova Categoria</h2>
-            <div className="flex flex-row w-full m-5">Formulaŕio</div>
+            <div className="flex flex-row w-full m-5">
+                <form>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Nome</label>
+                        <input type="text" id="name" name="name" placeholder="Nome Categoria" className="shadow appearance-none border-rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button type="submit"> Salvar</button>
+                    </div>
+                </form>
+            </div>
+
             <Button asChild>
                 <Link href="/categories/new">Voltar</Link>
             </Button>
