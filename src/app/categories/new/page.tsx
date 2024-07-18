ac7a851273b71d7f26c7c44effc76e10fd65d1bb
Gilberto Junior <giljrsantos@gmail.com>
2024-07-18 16:02:49 -0300

    feat: Estilizando o Título com @apply

--- src/app/categories/new/page.tsx
+++ src/app/categories/new/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function page(){
     return (
         <div>
-            <h2 className="text-cyan-950 text-sm font-semibold">Nova Categoria</h2>
+            <h2>Nova Categoria</h2>
             <div className="flex flex-row w-full m-5">Formulaŕio</div>
             <Button asChild>
                 <Link href="/categories/new">Voltar</Link>
