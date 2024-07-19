801bfd9d12820e95f8815f59e5502fc1d2fd3dd7
Gilberto Junior <giljrsantos@gmail.com>
2024-07-19 15:29:50 -0300

    feat: Preenchendo os Dados do Formulário

--- src/app/categories/edit/[id]/form.tsx
+++ src/app/categories/edit/[id]/form.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from "react";
+import Submit from "../../Submit";
 
 interface UpdateFormProps {
     data: {
@@ -10,11 +11,23 @@ interface UpdateFormProps {
 }
 
 export default function UpdateForm(props: UpdateFormProps){
+    const [name, setName] = useState(props.data.name);
+    function handleChange(event: any){
+        setName(event.target.value);
+    }
     const category = props.data;
     
     return (
         <div>
-            Formulário Categoria: { category.id } - {category.name}
+            <form>
+                <input type="hidden" name="id" value={props.data.id} />
+                <label>
+                    Nome:
+                    <input type="text" name="name" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 m-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" value={props.data.name} onChange={handleChange} />
+                </label>
+                <Submit>Salvar</Submit>
+            </form>
+            
         </div>
     )
 }
