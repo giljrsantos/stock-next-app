bc0f725b463569f58ee56fa7beb4c21516fcdcdb
Gilberto Junior <giljrsantos@gmail.com>
2024-07-19 11:34:43 -0300

    feat: Utilizando o Componente Table

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { PrismaClient } from '@prisma/client';
+import {
+    Table,
+    TableCaption,
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell
+} from '@/components/ui/table';
 
 export default async function page(){
     const prisma = new PrismaClient();
@@ -8,9 +17,33 @@ export default async function page(){
     const categories = await prisma.category.findMany()
     return (
         <div>
-            {categories.map((category) => (
-                <div key={category.id}>{ category.name }</div>
-            ))}
+            <Table>
+                <TableCaption>A list of your categories.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead className="w-[100px] text-center">Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {categories.map((category) => (
+                        <TableRow key={category.id}>
+                            <TableCell>{category.name}</TableCell>
+                            <TableCell className="text-right">
+                                <div className="flex flex-row gap-2">
+                                <Button asChild variant="link">
+                                        <Link href={`/categories/edit/${category.id}`}>Edit</Link>
+                                    </Button>                                    
+                                    <Button asChild variant="link">
+                                        <Link href={`/categories/del/${category.id}`}>Delete</Link>
+                                    </Button>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+
             <Button asChild>
                 <Link href="/categories/new">Nova Categoria</Link>
             </Button>
