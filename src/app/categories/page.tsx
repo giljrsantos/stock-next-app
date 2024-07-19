b761ce19ee7f9e1a38eac5eee82aaaf207b10687
Gilberto Junior <giljrsantos@gmail.com>
2024-07-19 11:18:16 -0300

    feat: Exibindo as Categorias

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { PrismaClient } from '@prisma/client';
 
-export default function page(){
+export default async function page(){
+    const prisma = new PrismaClient();
+
+    const categories = await prisma.category.findMany()
     return (
         <div>
+            {categories.map((category) => (
+                <div key={category.id}>{ category.name }</div>
+            ))}
             <Button asChild>
                 <Link href="/categories/new">Nova Categoria</Link>
             </Button>
