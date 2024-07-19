b052cc2ae6d22ba2d6b9eeaf3b3eb1975f75b457
Gilberto Junior <giljrsantos@gmail.com>
2024-07-19 15:37:44 -0300

    ref: refatorando codigo

--- src/app/categories/edit/[id]/form.tsx
+++ src/app/categories/edit/[id]/form.tsx
@@ -23,7 +23,7 @@ export default function UpdateForm(props: UpdateFormProps){
                 <input type="hidden" name="id" value={props.data.id} />
                 <label>
                     Nome:
-                    <input type="text" name="name" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 m-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" value={props.data.name} onChange={handleChange} />
+                    <input type="text" name="name" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 mt-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" value={props.data.name} onChange={handleChange} />
                 </label>
                 <Submit>Salvar</Submit>
             </form>
--- src/app/categories/new/page.tsx
+++ src/app/categories/new/page.tsx
@@ -27,16 +27,16 @@ export default function page(){
     return (
         <div>
             <h2>Nova Categoria</h2>
-            <div className="flex flex-row w-full m-5">
+            <div>
                 <form action={createCategory}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Nome</label>
+                    <div className="mb-4 w-full flex items-center justify-between">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 mr-3" htmlFor="name">Nome</label>
                         <input 
                             type="text" 
                             id="name" 
                             name="name" 
                             placeholder="Nome Categoria" 
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" />
+                            className="shadow appearance-none border rounded w-full py-2 px-3 mt-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" />
                     </div>
                     <div className="flex items-center justify-between">
                         <Button asChild variant="outline">
