afed9baee152b00308c406709d662f44c64ae64d
Gilberto Junior <giljrsantos@gmail.com>
2024-07-19 15:00:19 -0300

    feat: Editando uma Categoria e Definido um Tipo para o Parâmetro params

---
+++ src/app/categories/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+interface PageParams {
+    params: {
+        id: string
+    }
+}
+
+export default function page({params}: PageParams){
+    const id = params.id;
+
+    return <div>id: {id}</div>
+}
