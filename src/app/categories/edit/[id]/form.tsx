329e1fe22c0a6cc2c05503a0dccbe3cb6e296752
Gilberto Junior <giljrsantos@gmail.com>
2024-07-22 09:06:56 -0300

    feat: formatando codigo

--- src/app/categories/edit/[id]/form.tsx
+++ src/app/categories/edit/[id]/form.tsx
@@ -22,10 +22,8 @@ export default function UpdateForm(props: UpdateFormProps){
         <div>
             <form action={props.onSubmit}>
                 <input type="hidden" name="id" value={props.data.id} />
-                <label>
-                    Nome:
-                    <input type="text" name="name" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 mt-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" value={props.data.name} onChange={handleChange} />
-                </label>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name="name" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 mt-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]" value={props.data.name} onChange={handleChange} />
                 <Submit>Salvar</Submit>
             </form>
             
